3fdc2298c873f658442ad1b81d203a2957302746
Coria97 <santiagocoria@live.com.ar>
2023-03-04 19:55:24 -0300

    create ep add item

--- controllers/item_controller.php
+++ controllers/item_controller.php
@@ -1,5 +1,6 @@
 <?php
-
+  
+  require_once './models/category_model.php';
   require_once './models/item_model.php';
   require_once './views/item_view.php';
 
@@ -7,23 +8,32 @@
   {
     private $itemView;
     private $itemModel;
+    private $categoryModel;
 
     public function __construct()
     {
       $this->itemModel = new ItemModel();
       $this->itemView = new ItemView();
+      $this->categoryModel = new CategoryModel();
     }
 
     public function index()
     { 
+      $categories = $this->categoryModel->index();
       $items = $this->itemModel->index();
-      $this->itemView->index($items); 
+      $this->itemView->index($items, $categories); 
     }
 
     public function show($id)
     {
       $item = $this->itemModel->show($id);
       $this->itemView->show($item[0]);
+    }
+
+    public function create()
+    {
+      $this->itemModel->create($_POST);
+      $this->itemView->create();
     }
   }
 
